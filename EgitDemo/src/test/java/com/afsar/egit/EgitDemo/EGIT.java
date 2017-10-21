6e9f65b0db1cdbb61e9f035f91b6f1cb24735d1d
Afsar <Afsar@Afsar-PC>
2017-10-21 12:05:32 +0530

    first change

---
+++ EgitDemo/src/test/java/com/afsar/egit/EgitDemo/EGIT.java
@@ -0,0 +1,19 @@
+package com.afsar.egit.EgitDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EGIT {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Afsar\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		//
+		
+	
+	}
+
+}
